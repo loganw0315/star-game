17a80538ade2392c81c0a3e189d189a344131943
Logan Willis <loganw0315@gmail.com>
2021-12-15 13:45:48 -0600

    Moved StarMatch to new file, renamed Game file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import StarMatch from './StarMatch';
+import StarMatch from './Game';
 
 import reportWebVitals from './reportWebVitals';
 
